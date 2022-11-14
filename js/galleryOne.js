f83e66d19dd5711bd626399397afa362e4e27c95
Danuta Cybulska <danuta.cybulska.biz@gmail.com>
2022-11-14 18:33:19 +0100

    Add code js

---
+++ js/galleryOne.js
@@ -0,0 +1,13 @@
+function openPage(pageName,elemt,color){
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for(i=0;i<tabcontent.length;i++){
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for(i=0;i<tablinks.length; i++){
+       tablinks[i].style.backgroundColor = "";
+    }
+    document.getElementById(pageName).style.display = "block";
+    elemt.style.backgroundColor = color;
+}
