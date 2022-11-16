277a4127e54420a2cc5be5ba352950894dac7f67
Danuta Cybulska <danuta.cybulska.biz@gmail.com>
2022-11-16 13:20:43 +0100

    add function showGalleryTwo

--- js/galleryTwo.js
+++ js/galleryTwo.js
@@ -3,7 +3,7 @@ showGalleryTwo(item2);
 function goNextGall2(){
     plusSlides(1);
 }
-function goPrevGall1(){
+function goPrevGall2(){
     plusSlides(-1);
 }
 function plusSlides(n){
@@ -47,4 +47,24 @@ function showGalleryTwo(n){
     for(i = 0; i < dots.length; i++){
         dots[i].className = dots[i].className.replace(" active", "");
     }
+    slides[item2-1].style.display = "block";
+    dots[item2-1].className += " active";
+    captionText.innerHTML = dots[item2-1].alt;
 }
+var a_prev = document.getElementsByClassName("prev")[0];
+a_prev.addEventListener("click", goPrevGall2);
+var a_next = document.getElementsByClassName("next")[0];
+a_next.addEventListener("click", goNextGall2);
+
+var dot1 = document.getElementsByClassName("demo")[0];
+dot1.addEventListener("click",makeSlide1Gal2);
+var dot2 = document.getElementsByClassName("demo")[1];
+dot2.addEventListener("click",makeSlide2Gal2);
+var dot3 = document.getElementsByClassName("demo")[2];
+dot3.addEventListener("click",makeSlide3Gal2);
+var dot4 = document.getElementsByClassName("demo")[3];
+dot4.addEventListener("click",makeSlide4Gal2);
+var dot5 = document.getElementsByClassName("demo")[4];
+dot5.addEventListener("click",makeSlide5Gal2);
+var dot6 = document.getElementsByClassName("demo")[5];
+dot6.addEventListener("click",makeSlide6Gal2);
