f8ecd71779c2576c1628f0e6481adaa9b95d98d5
Danuta Cybulska <danuta.cybulska.biz@gmail.com>
2022-11-16 13:37:51 +0100

    add function next and prev

---
+++ js/galleryThree.js
@@ -0,0 +1,50 @@
+var display = document.querySelector(".img-display");
+var title = document.querySelector(".photo-title");
+var nxtBtn = document.querySelector(".next-btn");
+var prevBtn = document.querySelector(".prev-btn");
+
+var index =0;
+var images = [
+    {
+        title: "Woman 1",
+        url: 'images/woman 1.png'
+    },
+    {
+        title: "Woman 2",
+        url: 'images/woman 2.png'
+    },
+    {
+        title: "Woman 3",
+        url: 'images/woman 3.png'
+    },
+    {
+        title: "Woman 4",
+        url: 'images/woman 4.png'
+    },
+    {
+        title: "Woman 5",
+        url: 'images/woman 5.png'
+    },
+    {
+        title: "Woman 6",
+        url: 'images/woman 6.png'
+    }
+
+];
+
+function next(){
+    if(index === images.length-1){
+        index = 0;
+    } else {
+        index++;
+    }
+    return index;
+}
+function prev(){
+    if(index === 0){
+        index = images.length-1;
+    } else {
+        index--;
+    }
+    return index;
+}
